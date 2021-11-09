faf78a9ae64014964e055dc5a7c0d2c499563ee1
Ugo Henrique <ugohenrique@gmail.com>
2021-11-09 14:46:34 -0300

    Exemplo de pdo

---
+++ pdo_exemplo/insert.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ifsp";
+
+$nome = $_GET['nome'];
+$sobrenome = $_GET['sobrenome'];
+$email = $_GET['email'];
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+  VALUES (' $nome ' , ' $sobrenome ' ,  ' $email ')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "New record created successfully";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
