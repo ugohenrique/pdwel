37224a348225293bed28a34a44a11ba07f62ea86
Ugo Henrique <ugohenrique@gmail.com>
2021-11-09 19:41:36 -0300

    terça-feita
    alteração na string sql

--- pdo_exemplo/createDB.php
+++ pdo_exemplo/createDB.php
@@ -7,7 +7,7 @@ try {
   $conn = new PDO("mysql:host=$servername", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "CREATE DATABASE ifsp";
+  $sql = "CREATE DATABASE pdwel_terca";
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "Database created successfully<br>";
